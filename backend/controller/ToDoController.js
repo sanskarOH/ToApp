c5623dc47e51ea30fa60945b871696bc45372a58
Sanskar Diwedi <sanskaryeeha@gmail.com>
2023-10-06 20:07:05 +0530

    Added CRUD and completed backend

--- backend/controller/ToDoController.js
+++ backend/controller/ToDoController.js
@@ -2,8 +2,8 @@ const { log } = require('console')
 const ToDoModel = require('../models/ToDoModel')
 
 module.exports.getToDo = async (req, res) => {
-    const toDo = await ToDoModel.find()
-    res.send(toDo)
+  const toDo = await ToDoModel.find()
+  res.send(toDo)
 }
 
 // module.exports.saveToDo = async (req, res) => {
@@ -19,14 +19,30 @@ module.exports.getToDo = async (req, res) => {
 // }
 
 module.exports.saveToDo = async (req, res) => {
-    try {
-      const { text } = req.body;
-      const newToDo = await ToDoModel.create({ text });
-      console.log('Added Successfully...');
-      console.log(newToDo);
-      res.status(201).json(newToDo); // 201 Created status for successful creation
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'An error occurred while saving the todo item' });
-    }
-  };
+  try {
+    const { text } = req.body;
+    const newToDo = await ToDoModel.create({ text });
+    console.log('Added Successfully...');
+    console.log(newToDo);
+    res.status(201).json(newToDo); // 201 Created status for successful creation
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while saving the todo item' });
+  }
+};
+
+module.exports.updateTodo = async (req, res) => {
+  const { _id, text } = req.body
+  ToDoModel
+    .findByIdAndUpdate(_id, { text })
+    .then(() => res.send("Update successful.."))
+    .catch((err) => console.log(err))
+};
+
+module.exports.deleteToDo = async (req , res) => {
+  const {_id} = req.body
+  ToDoModel
+  .findByIdAndDelete(_id)
+  .then(()=>res.send("Deleted Successfully..."))
+  .catch((err)=>console.log(err))
+}
--- backend/routes/ToDoRoute.js
+++ backend/routes/ToDoRoute.js
@@ -1,8 +1,11 @@
 const {Router} = require('express');
-const { getToDo, saveToDo } = require('../controller/ToDoController');
+const { getToDo, saveToDo, updateTodo, deleteToDo } = require('../controller/ToDoController');
 const router = Router()
 
 router.get('/',getToDo)
 router.post('/save',saveToDo)
+router.post('/update',updateTodo)
+router.post('/delete',deleteToDo)
+
 
 module.exports= router;
